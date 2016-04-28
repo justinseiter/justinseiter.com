7699e463a03d98d927d97571c22dba005bfa511f
Justin Seiter <justinseiter@gmail.com>
2016-04-28 15:49:02 -0500

    Fix FOUT issue with Webpack ExtractTextPlugin.

--- app/index.js
+++ app/index.js
@@ -1,8 +1,1 @@
 require('./styles/index.styl');
-
-// Load the app :)
-document.addEventListener('DOMContentLoaded', function() {
-  var div = document.createElement('div');
-  div.id = 'app';
-  document.body.appendChild(div);
-});
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,28 @@
 // Register new plugin
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: './app/index.html',
-  inject: 'body'
-})
+  template: './app/index.html'
+});
+var ExtractTextPluginConfig = new ExtractTextPlugin("style.css", {allChunks: true})
 
 module.exports = {
-  entry: [
-    './app/index.js'
-  ],
+  entry: './app/index.js',
   output: {
     path: __dirname + '/dist',
     filename: 'bundle.js'
   },
   module: {
     loaders: [
-      { test: /\.styl$/, loader: "style-loader!css-loader!stylus-loader" },
+      {
+        test: /\.styl$/,
+        loader: ExtractTextPluginConfig.extract("style-loader", "css-loader!stylus-loader")
+      },
       { test: /\.png$/, loader: "file-loader" },
     ]
   },
-  plugins: [HtmlWebpackPluginConfig]
+  plugins: [
+    HtmlWebpackPluginConfig,
+    ExtractTextPluginConfig
+  ]
 }
